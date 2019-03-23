1fc6151e34b97338a6fb2aec0339030e84004a14
IreneKandidatova <47784868+IreneKandidatova@users.noreply.github.com>
2019-03-24 01:01:40 +0300

    add implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var H = 50;
+    var Q = 25;
+    var D = 10; 
+    var N = 5;
+    var P = 1;
+    var empty = [];
+    var error = "You are rich, my friend! We don't have so much coins for exchange";
+    var err = {"error":error};
+    if (currency > 10000) {return err};
+    if (currency <= 0) { return empty };
+    var HNumber = Math.floor(currency/H);
+    currency = currency -  HNumber*H;
+    var QNumber = Math.floor(currency / Q);
+    currency = currency - QNumber * Q;
+    var DNumber = Math.floor(currency / D);
+    currency = currency - DNumber * D;
+    var NNumber = Math.floor(currency / N);
+    currency = currency - NNumber * N;
+    var PNumber = Math.floor(currency / P);
+    var result = {"H": HNumber, "Q": QNumber, "D": DNumber, "N": NNumber,"P":PNumber};
+    if (PNumber == 0) { delete result.P};
+    if (NNumber == 0) { delete result.N};
+    if (DNumber == 0) { delete result.D};
+    if (QNumber == 0) { delete result.Q};
+    if (HNumber == 0) { delete result.H};
+   return result
 }
